d890ea355922b3a8de32abb49dc004f3fb959b52
Javokhir Fayzullaev <jofay91@gmail.com>
2026-02-22 11:51:05 +0500

    third commit

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import http from "http";
+import app from "./app.ts";
+
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`${process.env.PORT} ==>> comes from docker`);
+    console.log(`server is running on port ${PORT}🚀🚀`);
+})
