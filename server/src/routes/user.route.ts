6bec7afe0d461a2426cf118a7c8ce562207b5795
Javokhir Fayzullaev <jofay91@gmail.com>
2026-02-25 19:15:17 +0500

    25com2

--- server/src/routes/user.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { usersController } from '../controllers/user.controller.ts';
-
-const router = express.Router();
-router.post("/", usersController)
-
-
-export default router
